ee7396f15a5e99b652894d080bfd5d726caa5317
Jose Miranda <vidaljose2004@gmail.com>
2020-07-20 20:14:42 -0300

    ultimo comit con libreria database/sql

--- models/database.go
+++ models/database.go
@@ -5,18 +5,14 @@ import (
 	"log"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
+	"../config"
 )
 
 var db *sql.DB
 
-const username string = "root"
-const password string = ""
-const host string = "localhost"
-const port int = 3306
-const database string = "project_go_web"
-
 func CreateConnection(){
-	if connection, err := sql.Open("mysql",generateURL()) ; err != nil{
+	url := config.GetUrlDatabase()
+	if connection, err := sql.Open("mysql", url) ; err != nil{
 		panic(err)
 	}else{
 		db = connection
@@ -75,12 +71,6 @@ func existsTable(tableName string) bool{
 	sql := fmt.Sprintf("SHOW TABLES LIKE '%s'", tableName)
 	rows,_ := Query(sql)
 	return rows.Next() //next retorna true si esta bien 
-}
-	
-//conexion mysql
-//<username>:<password>@tcp(<host>:<port>)/<database>
-func generateURL() string{
-	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8",username,password,host,port,database)
 }
 
 	
--- test.go
+++ test.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"./models"
 )
 
 func main() {
 	models.CreateConnection()
 	models.Ping()
-	models.CreateTables()
-	
-	models.CreateUser("Gato1","MIMIU","miau_mi@gmail.com")
-	models.CreateUser("Gato2","MIMIU","miau_mi@gmail.com")
-	models.CreateUser("Gato3","MIMIU","miau_mi@gmail.com")
-	
-	//user := models.GetUser(2)
-	//fmt.Println(user)
-	
-	users := models.GetUsers()
-	fmt.Println(users)
-	
-	models.CloseConnection()
 }
